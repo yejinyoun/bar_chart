e0b257e6d48e537fbeeffdf08c59d5ab148fb7c7
yejinyoun <yejinyoun@icloud.com>
2023-02-10 13:05:43 +0100

    make sure the bars don't get generated with base setting when the model[i] is undefined

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 console.log("hihi!");
 
+let model = [32, 1, 2, 3];
+
 function init() {}
 
 function loop() {}
 
 function getNumberOfCustomers() {}
-
-let model = [32, 1, 2, 3];
 
 modifyModel();
 
@@ -16,12 +16,16 @@ function modifyModel() {
   // make it read one by one and create html and modify the height;
 
   for (let i = 0; i <= 39; i++) {
-    const bar = document.createElement("div");
-    bar.className = "bar";
-    bar.style.height = `${(300 / 32) * model[i]}px`;
-    document.querySelector("#barchart").appendChild(bar);
+    if (model[i] !== undefined) {
+      // if the model[i] is undefined, don't just generate bars w base setting
 
-    console.log(`${model[i]}customers and bar height is ${model[i] * (300 / 32)}px`);
+      const bar = document.createElement("div");
+      bar.className = "bar";
+      bar.style.height = `${(300 / 32) * model[i]}px`;
+      document.querySelector("#barchart").appendChild(bar);
+
+      console.log(`${model[i]}customers and bar height is ${model[i] * (300 / 32)}px`);
+    }
   }
 }
 
