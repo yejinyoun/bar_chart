13aeda93626c6ae9db66d725da925f1ee99cc7b2
yejinyoun <yejinyoun@icloud.com>
2023-02-10 13:50:06 +0100

    modify bar if : add 40 recent data of num of customers

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 console.log("hihi!");
 
 let model = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
   28, 29, 30, 31, 32, 1, 2, 3, 4, 5, 6, 7, 8,
 ];
 
-const customers = getNumberOfCustomers();
+modifyModel();
 
 modifyBars();
 
@@ -19,7 +19,21 @@ function getNumberOfCustomers() {
   return Math.floor(Math.random() * 32);
 }
 
-function modifyModel() {}
+function modifyModel() {
+  // change the data to 40 most recent data
+  // push the most recent customer number, and shift the oldest number
+  // need 40 data in the beginning and later on, make it push and shift one by one;
+
+  if (model.length <= 40) {
+    for (let i = 0; i <= 39; i++) {
+      // i = index of array;
+      const customer = getNumberOfCustomers();
+      model.push(customer);
+
+      console.log(`current customer data is${model} and most recent data is ${customer}`);
+    }
+  }
+}
 
 function modifyBars() {
   //i = index of each element in the model(array); model[0]
