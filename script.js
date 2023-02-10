d8bbafc033d48cfe442b801f6ffc117c2611c25c
yejinyoun <yejinyoun@icloud.com>
2023-02-10 14:13:21 +0100

    modifyModel, if model is less than 40 elements, create all random 40, if it's already 40 elements, push and shift

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 console.log("hihi!");
 
 let model = [
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
   28, 29, 30, 31, 32, 1, 2, 3, 4, 5, 6, 7, 8,
 ];
+
+console.log(model);
 
 modifyModel();
 
@@ -24,14 +26,19 @@ function modifyModel() {
   // push the most recent customer number, and shift the oldest number
   // need 40 data in the beginning and later on, make it push and shift one by one;
 
-  if (model.length <= 40) {
+  if (model.length < 40) {
     for (let i = 0; i <= 39; i++) {
       // i = index of array;
       const customer = getNumberOfCustomers();
-      model.push(customer);
+      model[i] = customer;
 
       console.log(`current customer data is${model} and most recent data is ${customer}`);
     }
+  } else if (model.length == 40) {
+    const customer = getNumberOfCustomers();
+    model.push(customer);
+    model.shift();
+    console.log(`current customer data is${model} and most recent data is ${customer}`);
   }
 }
 
