701bc8784249b9fab6aaa28b0b6b3e1b52fbc9b6
yejinyoun <yejinyoun@icloud.com>
2023-02-10 14:59:07 +0100

    edited explanation

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ console.log("hihi!");
 
 let model = [];
 
-console.log(model);
-
 init();
 
 function init() {
@@ -24,38 +22,31 @@ function getNumberOfCustomers() {
 }
 
 function modifyModel() {
-  // change the data to 40 most recent data
-  // push the most recent customer number, and shift the oldest number
-  // need 40 data in the beginning and later on, make it push and shift one by one;
+  // in the beginning, make 40 most recent data
+  // after, push the most recent data, and shift the oldest
 
   if (model.length < 40) {
     for (let i = 0; i <= 39; i++) {
       // i = index of array;
       const customer = getNumberOfCustomers();
+      // had to define customer inside this for loop or else it doesn't update each time;
       model[i] = customer;
-
-      console.log(`current customer data is${model} and most recent data is ${customer}`);
     }
   } else if (model.length == 40) {
     const customer = getNumberOfCustomers();
     model.push(customer);
     model.shift();
-    console.log(`current customer data is${model} and most recent data is ${customer}`);
   }
 }
 
 function modifyBars() {
   //i = index of each element in the model(array); model[0]
-  // make it read one by one and modify the height;
+  //get html element one by one and modify the height;
 
   for (let i = 0; i <= 39; i++) {
-    if (model[i] !== undefined) {
-      // if the model[i] is undefined, don't just generate bars w base setting
+    const bar = document.getElementById(`${i}`);
+    bar.style.height = `${(300 / 32) * model[i]}px`;
 
-      const bar = document.getElementById(`${i}`);
-      bar.style.height = `${(300 / 32) * model[i]}px`;
-
-      console.log(`${model[i]}customers and bar height is ${model[i] * (300 / 32)}px`);
-    }
+    console.log(`There are ${model[i]}customers and bar height is ${model[i] * (300 / 32)}px`);
   }
 }
